4b470e0823797355daac2010538eef4efa67c966
BenJarrett <b.cl.jarrett@gmail.com>
2021-08-23 20:09:11 -0500

    Createed Spy Repository, Model, and a method to get all spies and there properties.

---
+++ TeamHarrySpyDuhPoject/Controllers/SpyController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TeamHarrySpyDuhPoject.DataAccess;
+using TeamHarrySpyDuhPoject.Models;
+
+namespace TeamHarrySpyDuhPoject.Controllers
+{
+    [Route("api/spies")]
+    [ApiController]
+    public class SpyController : ControllerBase
+    {
+        private SpyRepository _repo;
+
+        public SpyController()
+        {
+            _repo = new SpyRepository();
+        }
+
+        [HttpGet]
+        public IActionResult GetAllSpies()
+        {
+            return Ok(_repo.GetAll());
+        }
+
+    }
+}
